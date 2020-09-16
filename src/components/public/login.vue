a819ca5244e120bee8a3c18f54ca1ae2776f251e
qingyou730 <2325582767@qq.com>
2020-09-16 23:06:15 +0800

    bg --login

--- src/components/public/login.vue
+++ src/components/public/login.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="login-user">
-        <h5 style="padding: 0; font-size: 24px;">登陆</h5>
-        <span class='span-login' @click="linkToReg">没有账号，点此处注册</span>
-        <el-input v-model="acountNum" placeholder="手机/邮箱" style="margin: 5px 0;"></el-input>
-        <el-input placeholder="请输入密码" v-model="password" show-password style="margin: 5px 0;"></el-input>
-        <div class="clearfix" style='margin: 0 0 4px 0;'>
-            <el-checkbox v-model="mermory" @change="mermoryFun">记住我</el-checkbox>
-        </div>
-        <el-button type="primary" style="width: 100%;margin-bottom: 6px;" @click="login">登陆</el-button>
-        <span style="display: block; width:100%;text-align: center; font-size:12px;margin-bottom: 4px;">已有账号，忘记密码</span>
-        <div style="width: 100%; height: 100px"></div>
-        <el-divider></el-divider>
-        <div class="other-login">
-            <el-button type="info" style="width:40px;height:40px" circle></el-button>
-            <el-button type="info" style="width:40px;height:40px" circle></el-button>
-            <el-button type="info" style="width:40px;height:40px" circle></el-button>
+    <div class="login-wrapper">
+        <div class="login-user">
+            <h5 style="padding: 0; font-size: 24px;">登陆</h5>
+            <span class='span-login'>没有账号，<span  @click="linkToReg"  style="cursor: pointer;color:#398FD3;">点此处注册</span></span>
+            <el-input v-model="acountNum" placeholder="手机/邮箱" style="margin: 5px 0;"></el-input>
+            <el-input placeholder="请输入密码" v-model="password" show-password style="margin: 5px 0;"></el-input>
+            <div class="clearfix" style='margin: 0 0 4px 0;'>
+                <el-checkbox v-model="mermory" @change="mermoryFun">记住我</el-checkbox>
+            </div>
+            <el-button  style="width: 100%;margin-bottom: 6px;background:#C1945D;" @click="login">登陆</el-button>
+            <span style="display: block; width:100%;text-align: center; font-size:12px;margin-bottom: 4px;">已有账号，忘记密码</span>
+            <div style="width: 100%; height: 80px"></div>
+            <div class="other-login-w">———其他登录方式———</div>
+            <div class="other-login">
+                <el-button class="back-image" style="width:40px;height:40px" circle></el-button>
+                <el-button class="back-image" style="width:40px;height:40px" circle></el-button>
+                <el-button class="back-image" style="width:40px;height:40px" circle></el-button>
+            </div>
         </div>
     </div>
 </template>
@@ -95,18 +97,17 @@ export default {
     position: fixed;
     top: 50%;
     left: 50%;
-    transform: translateX(-50%) translateY(-50%);
-    box-shadow: 1px 1px 19px #E2E2E2;
+    transform: translateX(-50%) translateY(-40%);
     border-radius: 5px;
     padding: 3vw;
     box-sizing: border-box;
     padding: 3vw;
+    background: #F2F2F2;
 }
 .login-user .span-login{
     font-size: 12px;
     display: block;
     text-align: right;
-    cursor: pointer;
 }
 .other-login{
     display: flex;
@@ -115,5 +116,23 @@ export default {
     box-sizing: border-box;
     padding: 0 5vw;
     height: 60px;
+}
+.back-image{
+    padding:0;
+    margin: 0;
+    background-size: cover;
+    background: url('/static/images/page/login/wx.png') no-repeat center;
+}
+.login-wrapper{
+    width:100vw;
+    height: 100vh;
+    background: url('/static/images/page/login/bg.png') no-repeat center;
+    background-size: cover;
+}
+.other-login-w{
+    text-align: center;
+    color: dimgrey;
+    font-size: 12px;
+    margin: 15px 0;
 }
 </style>
